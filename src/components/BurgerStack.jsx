9cf58f612b9276b913aabfb377d411b6b65bd03e
Zawadi2 <zawadin95@gmail.comm>
2024-08-01 18:12:27 -0500

    lifting lab

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BurgerStack = (props) => {
+  return (
+    <ul>
+      {props.stack.map((ingredient, index) => (
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
+          {ingredient.name}
+          <button onClick={() => removeFromBurger(ingredient)}>X</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default BurgerStack;
---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const IngredientList = (props) => {
+
+  return (
+    <ul>
+      {props.ingredients.map((ingredient, index) => (
+        <li key={index} style={{ backgroundColor: ingredient.color }}>
+          {ingredient.name}
+          <button onClick={() => addToBurger(ingredient)}>+</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default IngredientList;
